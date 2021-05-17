03e1fc3575f9d616349cda78f730c95261f412e3
stalaver <stalaver@ucsd.edu>
2021-05-16 22:08:15 -0700

    Finished part 2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Lab7_Solution
+Sergio Talavera
+Avinash Nayak
+
+# Part 1b Image
+![Part-1b](part-1b.png "Part-1b")
+
+# Part 2b Image
+![Part-2b](part-2b.png "Part-2b")
+
+# Link to Repository
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker
-// You will need 3 event listeners:
+/e/ You will need 3 event listeners:
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
